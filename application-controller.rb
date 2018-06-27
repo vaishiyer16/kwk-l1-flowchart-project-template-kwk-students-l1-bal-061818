8b96a255e1176b87b7c0d02de2d137f6966a17dd
vaishiyer16 <vaishnavi.l.iyer@gmail.com>
2018-06-27 20:15:56 +0000

    Done.

--- application-controller.rb
+++ application-controller.rb
@@ -8,37 +8,8 @@ class MyApp < Sinatra::Base
   erb :index
  end
 
- get '/questions' do
-   erb :questions
- end
-
- # get '/breakfast.erb' do
- #   erb :breakfast
+ # get '/questions' do
+ #   erb :questions
  # end
  #
- # get '/snack' do
- #   erb :snack
- # end
- #
- # get '/dessert' do
- #   erb :dessert
- # end
-
- post  '/results' do
-   answers = params.values
-   @total= 0
-   answers.each do |answer|
-     @total += answer.to_i
-   end
-   puts @total
-
-   @cookie = cookie_chooser(@total)
-   if @cookie == "cranberry"
-     erb :cranberry
-   elsif @cookie == "trailmix"
-     erb :trailmix
-   elsif @cookie == "chocolatey"
-     erb :chocolatey
-  end
-end
-end
+ # 
